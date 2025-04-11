8a2ed3237d11113a48cc0b0cf820be1f6e9ab328
yonas790 <yoni2752@gmail.com>
2025-04-11 22:31:06 +0300

    setting up server and connect to db

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 3000;
 
 const app = express();
+
+mongoose.connect(process.env.MONGOD_URL, {
+ }).then(() => {
+    console.log("DB connected");
+ }).catch((err) => {
+    console.error("DB connection error:", err);
+ });
 
 app.listen(PORT, "0.0.0.0", () => {
    console.log(`Server is running on http://localhost:${PORT}`);
